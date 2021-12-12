5474a6ea09886cbc596d3f14d24726f198038f05
Joseph Jayeoba <josephbusayojayeoba@gmail.com>
2021-12-12 07:07:43 -0800

    initial model and admin setup

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class wallpaper(models.Model):
+    img = models.ImageField(upload_to='')
+    description = models.CharField(max_length=150)
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
